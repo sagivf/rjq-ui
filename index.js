09a02f603194136ee680503e6d14cd7d38e9ed25
Sagiv Frankel <sagiv.f@seebo.com>
2017-05-09 16:11:32 +0300

    remove env vars

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const rjqApiKoa = require('rjq-api-koa')
 
 const {
   PORT = 4000,
-  RETHINK_PORT = 15157,
-  RETHINK_HOST = 'aws-eu-central-1-portal.0.dblayer.com',
-  RETHINK_USER = 'admin',
-  RETHINK_PASSWORD = 'DILlJZz2HlHLh3DQAAJuCOOCrnLc54ucmgs882-Bzgc'
+  RETHINK_PORT,
+  RETHINK_HOST,
+  RETHINK_USER,
+  RETHINK_PASSWORD
 } = process.env
 
 let {
