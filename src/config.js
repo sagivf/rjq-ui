765a9219d8da334c9f73b60c3fb4e069eb5c567e
Sagiv Frankel <sagivf@gmail.com>
2018-02-08 17:37:56 +0200

    fix again

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const API = '/rjq-ui/rjq-api'
+// const API = '/rjq-ui/rjq-api'
 // const API = 'http://localhost:4000/rjq-api'
-// const API = 'http://localhost:64244/rjq-ui/rjq-api'
+const API = 'http://localhost:58099/rjq-ui/rjq-api'
 // const API = 'http://rjq.spikli.me/rjq-ui/rjq-api'
 
 export default API
